eb5ef7da30fee49e6eeb204093477198b1c1ba15
quaso2222 <quaso2222@github.com>
2025-05-19 16:58:30 +0800

    assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -13,6 +13,8 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <time.h>
 
 #define PORT 9000
 #define DATA_FILE "/var/tmp/aesdsocketdata"
@@ -39,12 +41,46 @@ void signal_handler(int sig)
     }
 }
 
-void send_file_to_client(int client_fd, const char *file_path)
+struct message_file
 {
-    FILE *file = fopen(file_path, "r");
+    FILE *file;
+    pthread_mutex_t mutex;
+};
+
+int init_message_file(struct message_file *msg_file, char *file_name)
+{
+    msg_file->file = fopen(file_name, "a+");
+    if (msg_file->file == NULL)
+    {
+        syslog(LOG_ERR, "Failed to open file %s: %s", DATA_FILE, strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_init(&msg_file->mutex, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Failed to initialize mutex: %s", strerror(errno));
+        fclose(msg_file->file);
+        return -1;
+    }
+    return 0;
+}
+
+int cleanup_message_file(struct message_file *msg_file)
+{
+    if (msg_file->file != NULL)
+    {
+        fclose(msg_file->file);
+        msg_file->file = NULL;
+    }
+    pthread_mutex_destroy(&msg_file->mutex);
+    return 0;
+}
+
+void send_file_to_client(int client_fd, FILE *file)
+{
+
     if (file == NULL)
     {
-        syslog(LOG_ERR, "can not open file %s : %s", file_path, strerror(errno));
+        syslog(LOG_ERR, "can not open file : %s", strerror(errno));
         return;
     }
     char buffer[BUFFER_SIZE];
@@ -60,7 +96,7 @@ void send_file_to_client(int client_fd, const char *file_path)
                 if (errno == EINTR)
                     continue;
                 syslog(LOG_ERR, "Error sending data to client: %s", strerror(errno));
-                fclose(file);
+                // fclose(file);
                 return;
             }
             syslog(LOG_INFO, "sent %zu bytes to client", bytes_sent);
@@ -68,8 +104,15 @@ void send_file_to_client(int client_fd, const char *file_path)
             bytes_sent_total += bytes_sent;
         }
     }
-    fclose(file);
+    // fclose(file);
 }
+
+struct ThreadArgs
+{
+    int sid;
+    struct sockaddr_in *client_addr;
+    struct message_file *msg_file;
+};
 
 void daemonize()
 {
@@ -86,12 +129,215 @@ void daemonize()
         exit(EXIT_SUCCESS);
     }
 }
+
+void *client_connection(void *args)
+{
+    struct ThreadArgs *thread_args = (struct ThreadArgs *)args;
+    int sid = thread_args->sid;
+    struct sockaddr_in *client_addr = thread_args->client_addr;
+    struct message_file *msg_file = thread_args->msg_file;
+    int client_socket_fd = sid;
+    char client_ip_str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(client_addr->sin_addr), client_ip_str, INET_ADDRSTRLEN);
+    syslog(LOG_INFO, "Accepted connection from %s", client_ip_str);
+
+    // 处理客户端连接
+
+    // handle client connection begin
+
+    // Receives data over the connection and appends to file /var/tmp/aesdsocketdata, creating this file if it doesn’t exist.
+
+    // FILE *file = fopen("/var/tmp/aesdsocketdata", "a+");
+    // if (file == NULL)
+    // {
+    //     syslog(LOG_ERR, "fail to open file : %s", strerror(errno));
+    //     close(client_socket_fd);
+    //     syslog(LOG_INFO, "close connection from : %s", client_ip_str);
+    //     continue;
+    // }
+    // else
+    // {
+    //     syslog(LOG_INFO, "open file : %s", DATA_FILE);
+    // }
+
+    char *buffer_pointer = NULL;
+    int packet_buffer_cap = 0;
+    int packet_buffer_len = 0;
+    char recv_temp_buf[BUFFER_SIZE];
+    int bytes_received;
+
+    int connection_active = 1;
+    while (connection_active && !signal_recv)
+    {
+        syslog(LOG_INFO, "Begin receive from : %s", DATA_FILE);
+        bytes_received = recv(client_socket_fd, recv_temp_buf, sizeof(recv_temp_buf), 0);
+        if (bytes_received < 0)
+        {
+            if (errno == EINTR && signal_recv)
+                break;
+            if (errno == EINTR)
+                continue;
+            syslog(LOG_ERR, "error recving from %s : %s", client_ip_str, strerror(errno));
+            connection_active = 0;
+            break;
+        }
+        else
+        {
+            syslog(LOG_INFO, "Received %d bytes from %s", bytes_received, client_ip_str);
+        }
+        if (bytes_received == 0)
+        {
+            // 客户端关闭连接
+            connection_active = 0;
+            break;
+        }
+
+        // allocate buffer capacity
+        if (packet_buffer_len + bytes_received + 1 > packet_buffer_cap)
+        {
+            int new_cap = (packet_buffer_cap == 0) ? (packet_buffer_cap = 1) : (2 * packet_buffer_cap);
+            if (new_cap < packet_buffer_len + bytes_received + 1)
+            {
+                new_cap = packet_buffer_len + bytes_received + 1;
+            }
+            char *new_buf = realloc(buffer_pointer, new_cap);
+            if (new_buf == NULL)
+            {
+                syslog(LOG_ERR, "fail to allocate buffer for %s packet", client_ip_str);
+                buffer_pointer = NULL;
+                packet_buffer_cap = 0;
+                packet_buffer_len = 0;
+                connection_active = 0;
+                break;
+            }
+            buffer_pointer = new_buf;
+            packet_buffer_cap = new_cap;
+            syslog(LOG_INFO, "Buffer allocation complete : %d bytes", packet_buffer_cap);
+        }
+
+        memcpy(buffer_pointer + packet_buffer_len, recv_temp_buf, bytes_received);
+        packet_buffer_len += bytes_received;
+        buffer_pointer[packet_buffer_len] = '\0';
+        syslog(LOG_INFO, "test here1");
+        char *newline_char;
+        char *current_search_start = buffer_pointer;
+        while ((newline_char = strchr(current_search_start, '\n')) != NULL)
+        {
+            syslog(LOG_INFO, "test here2");
+            size_t packet_len_inc_newline = (newline_char - current_search_start) + 1;
+            syslog(LOG_INFO, "before _mutex_lock");
+            pthread_mutex_lock(&msg_file->mutex);
+            syslog(LOG_INFO, "CURRENT FILE POINTER : %p", msg_file->file);
+            syslog(LOG_INFO, "after _mutex_lock");
+
+            if (fwrite(current_search_start, 1, packet_len_inc_newline, msg_file->file) != packet_len_inc_newline)
+            {
+                syslog(LOG_ERR, "Error writing to %s: %s", DATA_FILE, strerror(errno));
+                connection_active = 0;
+                pthread_mutex_unlock(&msg_file->mutex);
+                break;
+            }
+            else
+            {
+                syslog(LOG_INFO, "write %zu bytes to %s", packet_len_inc_newline, DATA_FILE);
+            }
+
+            fflush(msg_file->file);
+            rewind(msg_file->file);
+            //
+            send_file_to_client(client_socket_fd, msg_file->file);
+            // send_file_to_client(client_socket_fd, "/home/linux/embeded/assignments-3-and-later-Quaso2222/server/testcontent");
+            syslog(LOG_INFO, "after send function");
+            fseek(msg_file->file, 0, SEEK_END);
+            pthread_mutex_unlock(&msg_file->mutex);
+            current_search_start = newline_char + 1;
+        }
+        if (!connection_active)
+            break;
+        syslog(LOG_INFO, "buffer pointer reset");
+        if (current_search_start > buffer_pointer && current_search_start <= buffer_pointer + packet_buffer_len)
+        {
+            size_t remaining_len = packet_buffer_len - (current_search_start - buffer_pointer);
+            memmove(buffer_pointer, current_search_start, remaining_len);
+            packet_buffer_len = remaining_len;
+            buffer_pointer[packet_buffer_len] = '\0';
+        }
+        else if (current_search_start >= buffer_pointer + packet_buffer_len)
+        {
+
+            packet_buffer_len = 0;
+            if (buffer_pointer)
+                buffer_pointer[0] = '\0';
+        }
+    }
+    free(buffer_pointer);
+    buffer_pointer = NULL;
+
+    syslog(LOG_INFO, "Closed connection from %s", client_ip_str);
+    close(client_socket_fd);
+    free(client_addr);
+    syslog(LOG_INFO, "msg_file check L279 : %p", msg_file);
+    syslog(LOG_INFO, "msg_file->file_check L280 : %p", msg_file->file);
+    client_addr = NULL;
+    client_socket_fd = -1;
+    return 0;
+    //  handle client connection end
+};
+
+void get_time(char *return_val, char *format)
+{
+    time_t t;
+    struct tm *tmp;
+
+    t = time(NULL);
+    tmp = localtime(&t);
+    if (tmp == NULL)
+    {
+        // perror("localtime");
+        syslog(LOG_ERR, "get local time fail: %s", strerror(errno));
+    }
+
+    if (strftime(return_val, 200, format, tmp) == 0)
+    {
+        // fprintf(stderr, "strftime returned 0");
+        syslog(LOG_ERR, "strftime time fail: %s", strerror(errno));
+    }
+    // printf("Result string is \"%s\"\n", outstr);
+}
+
+void *timer_thread(void *arg)
+{
+    struct timespec req, rem;
+    struct message_file *file = (struct message_file *)arg;
+
+    while (!signal_recv)
+    {
+        char time_str[200];
+
+        memset(time_str, 0, sizeof(time_str));
+        get_time(time_str, "%Y-%m-%d %H:%M:%S");
+
+        syslog(LOG_INFO, "get time : %s", time_str);
+        char *time_start = "timestamp:";
+        char combined_timestamp[300];
+        snprintf(combined_timestamp, sizeof(combined_timestamp), "%s%s\n", time_start, time_str);
+        pthread_mutex_lock(&file->mutex);
+        fputs(combined_timestamp, file->file);
+        fflush(file->file);
+        syslog(LOG_INFO, "write timestamp to file : %s", combined_timestamp);
+        pthread_mutex_unlock(&file->mutex);
+        req.tv_sec = 10; // 请求睡眠 10 秒
+        req.tv_nsec = 0; // 0 纳秒
+        nanosleep(&req, &rem);
+    }
+    return NULL;
+}
+
 int main(int argc, char *argv[])
 {
 
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-    int client_socket_fd = -1;
     char client_ip_str[INET_ADDRSTRLEN];
     openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
 
@@ -155,10 +401,26 @@ int main(int argc, char *argv[])
         daemonize();
     }
     // Listens for and accepts a connection
+
+    struct message_file *msg_file = malloc(sizeof(struct message_file));
+    if (init_message_file(msg_file, DATA_FILE) != 0)
+    {
+        syslog(LOG_ERR, "Failed to initialize message file");
+        // close(client_socket_fd);
+        // syslog(LOG_INFO, "close connection from : %s", client_ip_str);
+        return -1;
+    }
+    else
+    {
+        syslog(LOG_INFO, "open file : %s", DATA_FILE);
+    }
+    pthread_t timer_thread_id;
+    pthread_create(&timer_thread_id, NULL, timer_thread, (void *)msg_file);
     while (!signal_recv)
     {
-        syslog(LOG_INFO, "begin listen : %d", PORT);
+        int client_socket_fd = -1;
         client_socket_fd = accept(server_socket_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
         if (client_socket_fd != -1)
         {
             inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip_str, INET_ADDRSTRLEN);
@@ -167,140 +429,37 @@ int main(int argc, char *argv[])
         else
             continue;
 
-        // Receives data over the connection and appends to file /var/tmp/aesdsocketdata, creating this file if it doesn’t exist.
+        syslog(LOG_INFO, "msg_file check L381 : %p", msg_file);
+        syslog(LOG_INFO, "msg_file->file_check L382 : %p", msg_file->file);
+        pthread_t client_thread_id;
+        struct sockaddr_in *new_client_addr = malloc(sizeof(struct sockaddr_in));
+        *new_client_addr = client_addr;
 
-        FILE *file = fopen("/var/tmp/aesdsocketdata", "a+");
-        if (file == NULL)
+        struct ThreadArgs *args_for_thread = malloc(sizeof(struct ThreadArgs));
+        args_for_thread->sid = client_socket_fd;
+        args_for_thread->client_addr = new_client_addr;
+        args_for_thread->msg_file = msg_file;
+        syslog(LOG_INFO, "thread_args_POINTER_CHECK : %p", args_for_thread->msg_file->file);
+
+        int ret = pthread_create(&client_thread_id, NULL, client_connection, (void *)args_for_thread);
+        if (ret != 0)
         {
-            syslog(LOG_ERR, "fail to open file : %s", strerror(errno));
+            syslog(LOG_ERR, "Failed to create thread: %s", strerror(errno));
             close(client_socket_fd);
-            syslog(LOG_INFO, "close connection from : %s", client_ip_str);
+            free(new_client_addr);
             continue;
         }
-        else
-        {
-            syslog(LOG_INFO, "open file : %s", DATA_FILE);
-        }
-
-        char *buffer_pointer = NULL;
-        int packet_buffer_cap = 0;
-        int packet_buffer_len = 0;
-        char recv_temp_buf[BUFFER_SIZE];
-        int bytes_received;
-
-        int connection_active = 1;
-        while (connection_active && !signal_recv)
-        {
-            syslog(LOG_INFO, "Begin receive from : %s", DATA_FILE);
-            bytes_received = recv(client_socket_fd, recv_temp_buf, sizeof(recv_temp_buf), 0);
-            if (bytes_received < 0)
-            {
-                if (errno == EINTR && signal_recv)
-                    break;
-                if (errno == EINTR)
-                    continue;
-                syslog(LOG_ERR, "error recving from %s : %s", client_ip_str, strerror(errno));
-                connection_active = 0;
-                break;
-            }
-            else
-            {
-                syslog(LOG_INFO, "Received %d bytes from %s", bytes_received, client_ip_str);
-            }
-            if (bytes_received == 0)
-            {
-                // 客户端关闭连接
-                connection_active = 0;
-                break;
-            }
-
-            // allocate buffer capacity
-            if (packet_buffer_len + bytes_received + 1 > packet_buffer_cap)
-            {
-                int new_cap = (packet_buffer_cap == 0) ? (packet_buffer_cap = 1) : (2 * packet_buffer_cap);
-                if (new_cap < packet_buffer_len + bytes_received + 1)
-                {
-                    new_cap = packet_buffer_len + bytes_received + 1;
-                }
-                char *new_buf = realloc(buffer_pointer, new_cap);
-                if (new_buf == NULL)
-                {
-                    syslog(LOG_ERR, "fail to allocate buffer for %s packet", client_ip_str);
-                    buffer_pointer = NULL;
-                    packet_buffer_cap = 0;
-                    packet_buffer_len = 0;
-                    connection_active = 0;
-                    break;
-                }
-                buffer_pointer = new_buf;
-                packet_buffer_cap = new_cap;
-                syslog(LOG_INFO, "Buffer allocation complete : %d bytes", packet_buffer_cap);
-            }
-
-            memcpy(buffer_pointer + packet_buffer_len, recv_temp_buf, bytes_received);
-            packet_buffer_len += bytes_received;
-            buffer_pointer[packet_buffer_len] = '\0';
-
-            char *newline_char;
-            char *current_search_start = buffer_pointer;
-            while ((newline_char = strchr(current_search_start, '\n')) != NULL)
-            {
-                size_t packet_len_inc_newline = (newline_char - current_search_start) + 1;
-                if (fwrite(current_search_start, 1, packet_len_inc_newline, file) != packet_len_inc_newline)
-                {
-                    syslog(LOG_ERR, "Error writing to %s: %s", DATA_FILE, strerror(errno));
-                    connection_active = 0;
-                    break;
-                }
-                else
-                {
-                    syslog(LOG_INFO, "write %zu bytes to %s", packet_len_inc_newline, DATA_FILE);
-                }
-                fflush(file);
-                rewind(file);
-                //
-                send_file_to_client(client_socket_fd, DATA_FILE);
-                // send_file_to_client(client_socket_fd, "/home/linux/embeded/assignments-3-and-later-Quaso2222/server/testcontent");
-                syslog(LOG_INFO, "after send function");
-                fseek(file, 0, SEEK_END);
-
-                current_search_start = newline_char + 1;
-            }
-            if (!connection_active)
-                break;
-            syslog(LOG_INFO, "buffer pointer reset");
-            if (current_search_start > buffer_pointer && current_search_start <= buffer_pointer + packet_buffer_len)
-            {
-                size_t remaining_len = packet_buffer_len - (current_search_start - buffer_pointer);
-                memmove(buffer_pointer, current_search_start, remaining_len);
-                packet_buffer_len = remaining_len;
-                buffer_pointer[packet_buffer_len] = '\0';
-            }
-            else if (current_search_start >= buffer_pointer + packet_buffer_len)
-            {
-
-                packet_buffer_len = 0;
-                if (buffer_pointer)
-                    buffer_pointer[0] = '\0';
-            }
-        }
-        free(buffer_pointer);
-        buffer_pointer = NULL;
-        fclose(file);
-
-        syslog(LOG_INFO, "Closed connection from %s", client_ip_str);
-        close(client_socket_fd);
-        client_socket_fd = -1;
+        // init message file
 
         if (signal_recv)
             break;
     }
     syslog(LOG_INFO, "Server shutting down.");
-
-    if (client_socket_fd != -1)
-    {
-        close(client_socket_fd);
-    }
+    cleanup_message_file(msg_file);
+    // if (client_socket_fd != -1)
+    // {
+    //     close(client_socket_fd);
+    // }
 
     if (remove(DATA_FILE) != 0 && errno != ENOENT)
     {
